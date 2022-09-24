b8e40ff3160dd4873a07adfe559762c3fe29fd31
Ben Berman <ben.m.berman@gmail.com>
2022-09-24 09:07:29 -0400

    A stash commit before I go to Asheville

--- src/Stage/index.tsx
+++ src/Stage/index.tsx
@@ -3,3 +3,6 @@ export * from "./ReactStage";
 export * from "./Query";
 export * from "./Role";
 export * from "./Actor";
+
+// re-export the versison of zod this package uses for compatibility reasons
+export * as zod from "zod";
