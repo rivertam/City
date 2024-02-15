c9f17a449efcb2f14621d2e70aac91c5dab7d6d1
Ben Berman <ben.m.berman@gmail.com>
2024-02-15 13:08:47 -0500

    Set debugGraphs false

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom/client";
 
 import { Game } from "./Game";
 
-const debugGraphs = true;
+const debugGraphs = false;
 
 window.addEventListener("contextmenu", (e) => e.preventDefault());
 window.addEventListener("load", async () => {
